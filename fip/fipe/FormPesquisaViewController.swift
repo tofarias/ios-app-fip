d3587322e0e22421cec7529786d327e0e1ddb5d3
IOS SENAC <iossenac@FPAL152959.local>
2019-05-25 15:26:43 -0300

    Add Navegacao

---
+++ fip/fipe/FormPesquisaViewController.swift
@@ -0,0 +1,93 @@
+//
+//  FormPesquisaViewController.swift
+//  fip
+//
+//  Created by IOS SENAC on 25/05/19.
+//  Copyright © 2019 IOS SENAC. All rights reserved.
+//
+
+import UIKit
+
+class FormPesquisaViewController: UIViewController {
+    
+    var veiculo: Veiculos = .carros
+    
+    var veiculos: [Veiculo] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadNewVeiculo()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    // Funcao que configura web service
+    func loadNewVeiculo()
+    {
+        // Criando configuracao da sessao
+        let configuration = URLSessionConfiguration.default
+        
+        //Alterando propriedades da configuracao
+        configuration.waitsForConnectivity = true
+        //Criando sessao de configuracao
+        let session = URLSession(configuration: configuration)
+        let url = URL(string: "http://fipeapi.appspot.com/api/1/\(veiculo.rawValue)")!
+        let task = session.dataTask(with: url)
+        {(data, response, error) in
+            
+            //Quando terminar de executar o request cai aqui
+            guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200
+                else{
+                    return
+            }
+            
+            guard let data = data
+                else {
+                    return
+            }
+            
+            if let result = NSString(data: data, encoding: String.Encoding.utf8.rawValue) as String?
+            {
+                print(result)
+            }
+            do
+            {
+                let decoder = JSONDecoder()
+                let veiculos = try decoder.decode([Veiculo].self, from: data)
+                DispatchQueue.main.async{
+                    self.veiculos = veiculos
+                }
+            }catch{
+                print("Error\(error)")
+            }
+            
+            
+        }
+        task.resume()
+    }
+    
+    
+    
+    
+}
+
+
+struct Veiculo: Codable {
+    let nome: String
+    let fipeNome: String?
+    let id: Int
+    
+    
+    private enum CodingKeys : String, CodingKey
+    {
+        case nome = "name", fipeNome = "fipe_name", id
+    }
+}
+
+
+
+
+
+
---
+++ fip/fipe/MainViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  fip
+//
+//  Created by IOS SENAC on 25/05/19.
+//  Copyright © 2019 IOS SENAC. All rights reserved.
+//
+
+import UIKit
+
+
+
+class MainViewController: UIViewController {
+
+
+    
+    var veiculo: Veiculos = .carros
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func actionCaminhao(_ sender: UIButton) {
+        
+        veiculo = .caminhoes
+        performSegue(withIdentifier: "formpesquisa", sender: self)
+    }
+    
+
+    @IBAction func actionCarro(_ sender: UIButton) {
+        
+        veiculo = .carros
+        performSegue(withIdentifier: "formpesquisa", sender: self)
+    }
+    
+    @IBAction func actionMoto(_ sender: Any) {
+        
+        veiculo = .motos
+        performSegue(withIdentifier: "formpesquisa", sender: self)
+    }
+    
+    func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if (segue.identifier == "formpesquisa") {
+            
+            if let vc: FormPesquisaViewController = segue.destination as? FormPesquisaViewController {
+                vc.veiculo = veiculo
+            }
+            
+        }
+    }
+}
+
+enum Veiculos :String{
+    case caminhoes
+    case carros
+    case motos
+}
+
